49884e8f3ba6fc102499ed524b114c546550608e
gdv <gorbach.d.v@yandex.ru>
2019-03-24 14:09:10 +0300

    HW14 Pagination fix

--- src/ru/gorbach/hw14/common/solutions/paginationutils/PaginationUtils.java
+++ src/ru/gorbach/hw14/common/solutions/paginationutils/PaginationUtils.java
@@ -7,13 +7,17 @@ public class PaginationUtils {
     private PaginationUtils() {
     }
 
-    public static<T> List<T> getLimitList(List<T> values, Pagination pagination){
-        if (pagination.getOffset() > values.size()-1){
-            return Collections.emptyList();
+    public static <T> List<T> getLimitList(List<T> values, Pagination pagination) {
+        if (pagination == null) {
+            return values;
+        } else {
+            if (pagination.getOffset() > values.size() - 1) {
+                return Collections.emptyList();
+            }
+            int upperValue = pagination.getOffset() + pagination.getLimit();
+            return upperValue < values.size() ?
+                    values.subList(pagination.getOffset(), upperValue) :
+                    values.subList(pagination.getOffset(), values.size());
         }
-        int upperValue=pagination.getOffset()+pagination.getLimit();
-        return upperValue < values.size()?
-                values.subList(pagination.getOffset(), upperValue):
-                values.subList(pagination.getOffset(), values.size());
     }
 }
